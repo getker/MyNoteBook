4abca2a3071b1eceedd7456dd44954967a72f5bf
hankangwen <1397796710@qq.com>
2019-07-29 00:08:59 +0800

    基础：Roller A Ball.

---
+++ UnityProjects/PlanA/1.Base/RollABall/Assets/FollowTarget.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+    public Transform playerTran;
+
+    Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - playerTran.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = playerTran.position + offset;
+	}
+}
---
+++ UnityProjects/PlanA/1.Base/RollABall/Assets/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+    public float speed = 500f;
+    public Text text;
+    public GameObject winText;
+
+    int score = 0;
+    Rigidbody rgd;
+
+	// Use this for initialization
+	void Start () {
+        winText.SetActive(false);
+        rgd = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        rgd.AddForce(new Vector3(h, 0, v) * speed * Time.deltaTime);
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "PickUp")
+        {
+            score++;
+            text.text = score.ToString();
+            Destroy(collider.gameObject);
+            if(score >= 10)
+            {
+                winText.SetActive(true);
+            }
+        }
+    }
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    if(collision.collider.tag == "PickUp")
+    //    {
+    //        Destroy(collision.collider.gameObject);
+    //    }
+    //}
+}
