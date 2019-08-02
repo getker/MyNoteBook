5c18a3a34b7d2a91b247ecc5d177245cd97164aa
hankangwen <1397796710@qq.com>
2019-08-03 01:22:31 +0800

    新增一个不受切后台影响的倒计时方法。

---
+++ UnityProjects/TimeCountDown/Assets/Scripts/TimeCountDown.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCountDown : MonoBehaviour {
+    public Text timeText;
+    public int timeCount = 30;
+    public bool isRunCountDown = false; //开关
+
+    Int64 endTime;
+
+    void Awake(){
+        if(timeText == null){
+            timeText = GetComponent<Text>();
+        }
+        endTime = GetTimeStamp() + timeCount;
+        // Test
+        isRunCountDown = true;
+    }
+	
+	void Update () {
+        CountDown();
+    }
+
+    void CountDown(){
+        if (isRunCountDown){
+            int timeCount = (int)(endTime - GetTimeStamp());
+            if (timeCount < 0) {
+                timeCount = 0;
+                isRunCountDown = false;
+            }
+            timeText.text = timeCount.ToString();
+        }
+    }
+
+    Int64 GetTimeStamp()
+    {
+        TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        return Convert.ToInt64(ts.TotalSeconds);
+    }
+}
