9780840ea71430a363a6f6e52c4ac29bdfa89225
hankangwen <1397796710@qq.com>
2019-08-10 22:44:09 +0800

    新增unityClient。

---
+++ UnityProjects/UnityClientSocket/Assets/CSharpClient.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CSharpClient : MonoBehaviour {
+    public InputField hostInput;
+    public InputField portInput;
+    public Text recvText;
+    public string recvStr;
+    public Text clientText;
+    public InputField textInput;
+
+    Socket socket;
+    const int BUFFER_SIZE = 1024;
+    public byte[] readBuff = new byte[BUFFER_SIZE];
+
+    void Update()
+    {
+        recvText.text = recvStr;
+    }
+
+    public void Connetion()
+    {
+        recvText.text = "";
+        socket = new Socket(AddressFamily.InterNetwork,
+                            SocketType.Stream, ProtocolType.Tcp);
+        string host = hostInput.text;
+        int port = int.Parse(portInput.text);
+        socket.Connect(host, port);
+        clientText.text = "客户端地址 " + socket.LocalEndPoint.ToString();
+
+        socket.BeginReceive(readBuff, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCb, null);
+    }
+
+    void ReceiveCb(IAsyncResult ar)
+    {
+        try
+        {
+            int count = socket.EndReceive(ar);
+            string str = System.Text.Encoding.UTF8.GetString(readBuff, 0, count);
+            if (recvStr.Length > 300) recvStr = "";
+            recvStr += str + "\n";
+            socket.BeginReceive(readBuff, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCb, null);
+        }
+        catch (Exception e)
+        {
+            recvText.text += "连接已断开";
+            socket.Close();
+            throw;
+        }
+    }
+
+    public void Send()
+    {
+        string str = textInput.text;
+        byte[] bytes = System.Text.Encoding.Default.GetBytes(str);
+        try
+        {
+            socket.Send(bytes);
+        }
+        catch
+        {
+        }
+    }
+}
